ef3f47916059e4c9c81a6081941071f828b99ef6
smitpandya3873 <155702858+smitpandya3873@users.noreply.github.com>
2024-12-06 16:09:06 +0530

    variable

---
+++ Day1/index.js
@@ -0,0 +1,9 @@
+// console.log("Hello world!");
+// console.log("one2345");
+
+let text = document.getElementById("text");
+document.write("<h1>hello Word</h1>");
+//alert("Hello world!");
+
+text.innerHTML = "Hello world!";
+// text.innerText = "<h1>Hello Workd</h1>";
---
+++ Day2/day2.js
@@ -0,0 +1,89 @@
+// what is variables
+// understanding data types
+// Types Of Operator
+// Arithmetic operators
+// Assignment operators
+// Comparison operators
+// Logical operators
+// conversion with console.log and innerHTML
+
+// let var const
+
+// javascript is a dynamic type language
+
+//number
+let num1 = 123;
+let num2 = 456;
+let num3 = 789;
+let num4 = 741;
+
+console.log(num1, num2, num3, num4);
+console.log(typeof num1);
+console.log(typeof num2);
+console.log(typeof num3);
+console.log(typeof num4);
+
+//string
+
+let string1 = "hello world";
+let string2 = "smit";
+let string3 = "dfg";
+let string4 = "iop";
+
+console.log(string1, string2, string3, string4);
+console.log(typeof string1);
+console.log(typeof string2);
+console.log(typeof string3);
+console.log(typeof string4);
+
+//boolean
+let boolean = true;
+let boolean2 = false;
+let boolean3 = true;
+let boolean4 = false;
+let boolean5 = true;
+
+console.log(boolean, boolean2, boolean3, boolean5);
+console.log(typeof boolean);
+console.log(typeof boolean2);
+console.log(typeof boolean3);
+console.log(typeof boolean4);
+
+//object
+let object1 = { Name: "smit", surname: "pandya" };
+let object2 = { Name: "dhruv", surname: "vachhani" };
+let object3 = { Name: "smit ", surname: "gopani" };
+let object4 = { Name: "rohit", surname: "chhangani" };
+
+console.log(object1);
+console.log(typeof object1);
+console.log(object2.Name);
+
+// bigInt //11112333323546547
+let value = 1234509823409802938509286098243098324n;
+console.log(value);
+
+let username = null;
+console.log(username);
+console.log(typeof username);
+
+let array = ["smit", "pandya", 458];
+console.log(array[0]);
+console.log(array);
+
+function print() {
+  console.log("Hello World");
+}
+function print1() {
+  console.log("Hello World");
+}
+function print2() {
+  console.log("Hello World");
+}
+function print3() {
+  console.log("Hello World");
+}
+print();
+print1();
+print2();
+print3();
