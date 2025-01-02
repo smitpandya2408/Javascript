3774a19b3cc43eaac1da7680b6965755db684c30
smitpandya3873 <155702858+smitpandya3873@users.noreply.github.com>
2025-01-02 13:40:05 +0530

    countdown

---
+++ error_handling/script.js
@@ -0,0 +1,17 @@
+setTimeout(() => {
+  console.log("Hello smit pandya");
+}, 1000);
+setTimeout(() => {
+  try {
+    console.log(rahul);
+  } catch (e) {
+    console.log("Rahul is absent now");
+  }
+});
+setTimeout(() => {
+  console.log("How's your day ?");
+}, 1000);
+
+setTimeout(() => {
+  console.log("I hope you are very well now");
+}, 1000);
